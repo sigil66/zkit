63786d3db66be5bff10b74a21328a8f3ee1ad793
Zachary Schneider <zachary.schneider@instana.com>
2018-06-28 23:42:06 +0200

    flesh out sh

--- provider/sh_unix.go
+++ provider/sh_unix.go
@@ -3,6 +3,9 @@ package provider
 import (
 	"github.com/solvent-io/zkit/action"
 	"context"
+	"os/exec"
+	"bufio"
+	"fmt"
 )
 
 type ShUnix struct {
@@ -23,7 +26,29 @@ func (s *ShUnix) Realize(phase string, ctx context.Context) (string, error) {
 }
 
 func (s *ShUnix) exec(ctx context.Context) (string, error) {
+	cmd := exec.Command(s.sh.Cmd[0], s.sh.Cmd[1:]...)
+	cmdReader, err := cmd.StdoutPipe()
+	if err != nil {
+		return "", err
+	}
 
-	return "execin the world bro!", nil
+	scanner := bufio.NewScanner(cmdReader)
+	go func() {
+		for scanner.Scan() {
+			fmt.Printf("    %s\n", scanner.Text())
+		}
+	}()
+
+	err = cmd.Start()
+	if err != nil {
+		return "", err
+	}
+
+	err = cmd.Wait()
+	if err != nil {
+		return "", err
+	}
+
+	return "", nil
 }
 
